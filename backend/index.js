3ba2eefa39de24998790c0fd777df78376e08c1c
divyanshchoudhary01 <divyanshchoudhary565@gmail.com>
2023-07-05 14:59:10 +0530

    modified:   backend/index.js 	new file:   backend/node_modules/path/.npmignore 	new file:   backend/node_modules/path/LICENSE 	new file:   backend/node_modules/path/README.md 	new file:   backend/node_modules/path/package.json 	new file:   backend/node_modules/path/path.js 	new file:   backend/node_modules/process/.eslintrc 	new file:   backend/node_modules/process/LICENSE 	new file:   backend/node_modules/process/README.md 	new file:   backend/node_modules/process/browser.js 	new file:   backend/node_modules/process/index.js 	new file:   backend/node_modules/process/package.json 	new file:   backend/node_modules/process/test.js 	new file:   backend/node_modules/util/LICENSE 	new file:   backend/node_modules/util/README.md 	new file:   backend/node_modules/util/node_modules/inherits/LICENSE 	new file:   backend/node_modules/util/node_modules/inherits/README.md 	new file:   backend/node_modules/util/node_modules/inherits/inherits.js 	new file:   backend/node_modules/util/node_modules/inherits/inherits_browser.js 	new file:   backend/node_modules/util/node_modules/inherits/package.json 	new file:   backend/node_modules/util/package.json 	new file:   backend/node_modules/util/support/isBuffer.js 	new file:   backend/node_modules/util/support/isBufferBrowser.js 	new file:   backend/node_modules/util/util.js 	modified:   backend/package-lock.json 	modified:   backend/package.json

--- backend/index.js
+++ backend/index.js
@@ -3,17 +3,17 @@ const dotenv=require('dotenv').config()
 const mongoose=require('mongoose')
 const contactController=require('./controllers/contact')
 const Contact=require('./models/Contact')
-
+const path=require("path")
 const app=express()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 //connect db
 //connect servercls
-app.use(express.static(path.join(__dirname,"./backend/build")))
+app.use(express.static(path.join(__dirname,"./build")))
 
 mongoose.connect(process.env.MONGODB_URL)
 app.get("*",(req,res)=>{
-res.sendFile(path.join(__dirname,"./backend/build/index.html"))
+res.sendFile(path.join(__dirname,"./build/index.html"))
 
 })
 
